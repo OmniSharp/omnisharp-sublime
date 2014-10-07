a62109e45f4ca8d7ca1492f29fb93a0edc08b5e7
Nathan Black <nathan@nathanblack.org>
2014-10-07 00:37:35 -0700

    Add a highlight hander to Find Usages that opens the file to allow the user to preview the entire file

--- commands/find_usages.py
+++ commands/find_usages.py
@@ -7,19 +7,25 @@ from ..lib import helpers
 
 class OmniSharpFindUsages(sublime_plugin.TextCommand):
     def run(self, edit):
-        omnisharp.get_response(self.view, '/findusages', lambda data: self._show_usages(data))
+        omnisharp.get_response(self.view, '/findusages', self._show_usages)
 
     def _show_usages(self, data):
-        if "QuickFixes" in data and data["QuickFixes"] is not None:
-            usages = data["QuickFixes"]
-            items = [[u["Text"].strip(), u["FileName"]] for u in usages]
-            window = self.view.window()
+        if data is None or data['QuickFixes'] is None:
+            return
 
-            def on_done(i):
-                if i is not -1:
-                    window.open_file('{}:{}:{}'.format(usages[i]["FileName"], usages[i]["Line"] or 0, usages[i]["Column"] or 0), sublime.ENCODED_POSITION)
+        usages = data["QuickFixes"]
+        items = [[u["Text"].strip(), u["FileName"]] for u in usages]
+        window = sublime.active_window()
 
-            window.show_quick_panel(items, on_done)
+        def on_done(i):
+            if i is not -1:
+                window.open_file('{}:{}:{}'.format(usages[i]["FileName"], usages[i]["Line"] or 0, usages[i]["Column"] or 0), sublime.ENCODED_POSITION)
+
+        def on_highlight(i):
+            if i is not -1:
+                window.open_file('{}:{}:{}'.format(usages[i]["FileName"], usages[i]["Line"] or 0, usages[i]["Column"] or 0), sublime.ENCODED_POSITION | sublime.TRANSIENT)
+
+        window.show_quick_panel(items, on_done, on_highlight=on_highlight)
 
     def is_enabled(self):
         return helpers.is_csharp(self.view)
