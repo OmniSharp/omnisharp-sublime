8abc159ee6a36690a72af530a9ce4b239fd0fdc0
Jonathan Channon <jonathan.channon@gmail.com>
2014-10-12 22:30:18 +0100

    list dlls in quick panel rather than typing absolute path

--- commands/add_reference.py
+++ commands/add_reference.py
@@ -1,35 +1,36 @@
 import time
 import sublime
 import sublime_plugin
+import fnmatch
+import os
 
 from ..lib import omnisharp
 from ..lib import helpers
 
-
 class OmniSharpAddReference(sublime_plugin.TextCommand):
-    data = None
-
+    
     def run(self, edit):
-        if self.data is None:
-            sublime.active_window().show_input_panel("Add Reference:", "", 
-                self._addref_inputed, None, None
-            )
+        parentpath =  helpers.current_project_folder(self.view)
+        matches = []
+        for root, dirnames, filenames in os.walk(parentpath):
+          if 'bin' not in root or 'obj' not in root:
+            for filename in fnmatch.filter(filenames, '*.dll'):
+              matches.append(os.path.join(root, filename))
+
+        def on_done(i):
+            if i is not -1:
+                params = {'reference': matches[i]}
+                omnisharp.get_response(
+                    self.view, '/addreference', self._process_addref, params)
+
+        if len(matches) > 0:
+            sublime.active_window().show_quick_panel(matches, on_done)
         else:
-            self._process_addref(edit)
+            sublime.status_message('No libraries found to add reference to')
 
-    def _addref_inputed(self, reffilename):
-        params = {'reference': reffilename}
-        omnisharp.get_response(
-            self.view, '/addreference', self._addref_response_received, params)
 
-    def _addref_response_received(self, data):
-        self.data = data
-        self.view.run_command('omni_sharp_add_reference')
-
-    def _process_addref(self, edit):
-        data = self.data
+    def _process_addref(self, data):
         sublime.status_message(data['Message'])
-        self.data = None
 
     def is_enabled(self):
         return helpers.is_csharp(self.view)
