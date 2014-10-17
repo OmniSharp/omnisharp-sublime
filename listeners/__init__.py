7a057503912058765f4e420ed8d2f62c680a8f87
Jonathan Channon <jonathan.channon@gmail.com>
2014-10-17 20:18:20 +0100

    add file to csproj

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -2,9 +2,11 @@ from .completion import OmniSharpCompletionEventListener
 from .server_runner import OmniSharpServerRunnerEventListener
 from .syntax import OmniSharpSyntaxEventListener
 from .overridelistener import OmniSharpOverrideListener
+from .add_file_to_project import OmniSharpAddFileToProjectEventListener
 __all__ = [
     'OmniSharpCompletionEventListener',
     'OmniSharpServerRunnerEventListener',
     'OmniSharpSyntaxEventListener',
-    'OmniSharpOverrideListener'
+    'OmniSharpOverrideListener',
+    'OmniSharpAddFileToProjectEventListener'
 ]
---
+++ listeners/add_file_to_project.py
@@ -0,0 +1,23 @@
+import os
+import sublime
+import sublime_plugin
+
+from ..lib import helpers
+from ..lib import omnisharp
+
+
+class OmniSharpAddFileToProjectEventListener(sublime_plugin.EventListener):
+
+    def on_post_save(self, view):
+        if not helpers.is_csharp(view):
+            return
+        
+        self.view = view
+
+        omnisharp.get_response(view, '/addtoproject', self._handle_addtoproject)
+
+
+    def _handle_addtoproject(self, data):
+        print('file added to project')
+
+
