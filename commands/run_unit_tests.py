19ddc07ebc8878d420844dcd6ccb33c356b527fd
Jonathan Channon <jonathan.channon@gmail.com>
2014-10-26 19:13:03 +0000

    tests complete

--- commands/run_unit_tests.py
+++ commands/run_unit_tests.py
@@ -6,17 +6,16 @@ from ..lib import helpers
 from ..lib import omnisharp
 
 class OmniSharpRunUnitTests(sublime_plugin.TextCommand):
-    def run(self, edit):
-        omnisharp.get_response(self.view, '/gettestcontext', self._handle_rununittests)
+    
+    def run(self, edit, testtype='all'):
+        print(testtype)
+        params = {}
+        params["type"] = testtype
+        omnisharp.get_response(self.view, '/gettestcontext', self._handle_rununittests, params)
 
     def is_enabled(self):
         return helpers.is_csharp(sublime.active_window().active_view())
 
     def _handle_rununittests(self, data):
-    	
         testcommand = data["TestCommand"]
-        commands = testcommand.split(' ')
-        if os.name == 'posix':
-        	sublime.active_window().run_command('exec',{"cmd":['mono',commands[0], commands[1].replace('"','')]})
-        # else:
-        #     sublime.active_window().run_command('exec','')
+        sublime.active_window().run_command('exec',{"cmd":[testcommand], "shell":"true"})
