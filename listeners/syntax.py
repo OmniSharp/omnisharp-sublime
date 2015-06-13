b3279f79efefe7f1e0b8bda72a3be49219e23151
fishg <fish@jidanke.com>
2015-06-13 22:07:56 +0800

    output error log order by line number and column

--- listeners/syntax.py
+++ listeners/syntax.py
@@ -60,6 +60,7 @@ class OmniSharpSyntaxEventListener(sublime_plugin.EventListener):
         oops_map = {}
 
         if "QuickFixes" in self.data and self.data["QuickFixes"] != None and len(self.data["QuickFixes"]) > 0:
+            self.data["QuickFixes"].sort(key = lambda a:(a['Line'],a['Column']))
             for i in self.data["QuickFixes"]:
                 point = self.view.text_point(i["Line"]-1, i["Column"]-1)
                 reg = self.view.word(point)
